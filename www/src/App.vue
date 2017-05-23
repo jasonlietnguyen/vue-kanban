9f8e67423b9b5f6e7cd6051d968b5ad00eb5d748
Nathan Elg <elgn@students.wwwu.edu>
2017-05-23 16:31:59 -0600

    container added

--- www/src/App.vue
+++ www/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <error></error>
-    <router-view></router-view>
+    <div class="container">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
