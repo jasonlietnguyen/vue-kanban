120c6edce5c8dd951114ded650f2b3fe9b0ec0e1
jasonlietnguyen@gmail.com <jason123nguyen@gmail.com>
2017-05-23 17:34:53 -0600

    add nav

--- www/src/components/Homepage.vue
+++ www/src/components/Homepage.vue
@@ -1,6 +1,39 @@
 <template>
   <div class="homepage">
-
+    <nav class="navbar navbar-toggleable-md navbar-light">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+    <a class="navbar-brand" href="#">
+    <img src="https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/trello-header-logos/af7af6ed478d3460709d715d9b3f74a4/trello-logo-white.svg" alt="" width="150px">
+  </a>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav navbar-toggler-right">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Sign In</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Log In</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</nav>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8">
+        <h2 class="title">Trello lets you work more <br> collaboratively and get more done.</h2>
+        <h5>Trello’s boards, lists, and cards enable you to organize and prioritize your projects in a fun, flexible and rewarding way.</h5>
+        <br>
+        <p>Already use Trello? Log in.</p>
+        <button type="button" class="btn btn-success">Sign Up - It's Free</button>
+        <img src="https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/155358fcb5830aa596ac191b15bb0bb2/trello-collaborate-home.png" alt="" width="800px">
+      </div>
+      <div class="col-md-2">
+      </div>
+    </div>
+    div.row
+  </div>
 
   </div>
 </template>
@@ -20,5 +53,29 @@ export default {
 
 
 <style scoped>
-
+i{
+  color: white;
+}
+.logo{
+  color: white;
+  font-size: 40px;
+}
+h2{
+  margin-top: 100px;
+  color: white;
+  font-weight: 900;
+  text-align: left;
+}
+h5{
+  margin-top: 20px;
+  color: white;
+  text-align: left;
+}
+p{
+  text-align: left;
+  color: white;
+}
+button{
+  margin-left: 0
+}
 </style>
