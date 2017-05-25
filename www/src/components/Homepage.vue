271a313c21c8bb74f4aff7a40bbf7382b686d299
jasonlietnguyen@gmail.com <jason123nguyen@gmail.com>
2017-05-25 15:35:00 -0600

    fixed button

--- www/src/components/Homepage.vue
+++ www/src/components/Homepage.vue
@@ -1,39 +1,60 @@
 <template>
-  <div class="navbar">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-12">
-          <ul class="list-inline">
-            <li class="">
-              <router-link class="btn" to="/register">Register</router-link>
-            </li>
-            <li class="">
-              <router-link class="btn" to="/login">Log In</router-link>
-            </li>
-          </ul>
-        </div>
+  <div class="homepage">
+    <nav class="navbar navbar-toggleable-md navbar-light">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+    <a class="navbar-brand" href="#">
+    <img src="https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/trello-header-logos/af7af6ed478d3460709d715d9b3f74a4/trello-logo-white.svg" alt="" width="150px">
+  </a>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav navbar-toggler-right">
+      <li class="nav-item">
+        <button type="button" class="btn btn-success"><router-link class="btn" to="/register">Register</router-link></button>      
+      </li>
+      <li class="nav-item">
+        <button type="button" class="btn btn-primary"><router-link class="btn" to="/login">Log In</router-link></button>
+      </li>
+    </ul>
+  </div>
+</nav>
+</nav>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8">
+        <h2 class="title">Trello lets you work more <br> collaboratively and get more done.</h2>
+        <h5>Trello’s boards, lists, and cards enable you to organize and prioritize your projects in a fun, flexible and rewarding way.</h5>
+        <br>
+        <button type="button" class="btn btn-success">Sign Up - It's Free</button><br><br>
+        <p>Already use Trello? <a href="http://localhost:8080/#/login">Log In.</a></p>
       </div>
     </div>
+    <div class="row">
+      <div class="col-md-8 offset-md-4">
+          <img src="https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/155358fcb5830aa596ac191b15bb0bb2/trello-collaborate-home.png" alt="" width="800px">
+      <br><br><br>
+      </div>
+    </div>
+  </div>
+
   </div>
 </template>
 
 
 <script>
-  export default {
-    name: 'homepage',
-    computed: {},
-    methods: {},
-    components: {}
-  }
+export default {
+  name: 'homepage',
+  data(){
 
+  },
+  computed:{},
+  methods:{},
+  components:{}
+}
 </script>
 
 
 <style scoped>
-.list-inline li{
-  display: inline-block;
-  margin: 0 3px;
-}
 i{
   color: white;
 }
